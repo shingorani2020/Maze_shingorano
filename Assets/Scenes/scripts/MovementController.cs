d8fd19a0340875cf7257d638283a08e4c69f2648
Serena Hingorani <shingorani2020@chadwickschool.org>
2019-02-20 09:14:07 -0800

    Add Feat : Intial rig setup

---
+++ Assets/Scenes/scripts/MovementController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+  //ublic variables can be set in the inspector
+  public GameObject startObj;
+
+  //priavte Var
+  private CharacterController cc ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //position the Camera node at the start location
+      transform.position = startObj.transform.position;
+      cc = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
