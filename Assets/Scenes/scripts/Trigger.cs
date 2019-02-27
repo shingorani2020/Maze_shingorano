32821b6b02b3c6a28a5ae49841e1db2b68ea941e
Serena Hingorani <shingorani2020@chadwickschool.org>
2019-02-27 08:00:50 -0800

    Add: feat pellets w/beginning code

---
+++ Assets/Scenes/scripts/Trigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+  //public varibales
+  public string sendToName;
+  public string call;
+  public string key;
+  public bool delete;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+      Debug.Log(Other.gameObject.name + "hit" + this.gameObject.name);
+      GameObject.Find(sendtoName).SendMessage(call,key);
+      if (delete)
+      {
+        Destrou(this.gameObject);
+      }
+
+    }
+}
